b9beb496dba8f688c1e24ed5bd3ffba841855fa4
anton <haglundanton+github@gmail.com>
2013-05-03 23:09:02 +0200

    die when no ping
    if the bot doesn't get PING from server within 900 s it will roll over and die.
    
    taking the oppertunity to tidy up the stdout-logging as well.
    
    Signed-off-by: anton <haglundanton+github@gmail.com>

--- urlbot.py
+++ urlbot.py
@@ -6,6 +6,7 @@ from urlparse import urlparse
 import urllib2
 import re
 import htmllib
+import time
 
 HOST='irc.eagle.y.se'
 PORT=6667
@@ -16,8 +17,11 @@ CHANNELINIT='#testurltitle'
 readbuffer=''
 s=socket.socket( )
 s.connect((HOST, PORT))
-print s.recv(4096)
+print int(time.time()),s.recv(4096)
+
+print int(time.time()),'NICK '+NICK
 s.send('NICK '+NICK+'\n')
+print int(time.time()),'USER '+IDENT+' '+HOST+' bla :'+REALNAME
 s.send('USER '+IDENT+' '+HOST+' bla :'+REALNAME+'\n')
 
 def unescape(s):
@@ -32,8 +36,8 @@ def fetchtitle(url):
     try:
         page = urllib2.urlopen(url)
     except urllib2.URLError, e:
-        print 'Error code: ' + str(e.code)
-        print "I'm sorry dave..."
+        print int(time.time()),'Error code: ' + str(e.code)
+        print int(time.time()),"I'm sorry dave..."
         return ""
 
     for html in page.readlines():
@@ -67,20 +71,31 @@ def parsemsg(msg):
     for w in msgpart.split(' '):
         o = urlparse(w)
         if len(o.netloc)!=0:
+            print int(time.time()),'checking url '+w.rstrip('\n')
             title = fetchtitle(w)
             if len(title)!=0:
+                print int(time.time()),'PRIVMSG '+info[2]+' :'+title
                 s.send('PRIVMSG '+info[2]+' :'+title+'\n')
 
+
+timer = time.time()
 while True:
     line=s.recv(512)
-    print line
+    print int(time.time()),line.rstrip('\n')
     if line.find('Closing Link')!=-1:
+        print int(time.time()),'closing link, exiting..'
         sys.exit(1)
     if line.find('Welcome to the Internet Relay Network')!=-1: #FIXME
+        print int(time.time()),'JOIN '+CHANNELINIT
         s.send('JOIN '+CHANNELINIT+'\n')
     if line.find('PRIVMSG')!=-1:
         parsemsg(line)
     line=line.rstrip()
     line=line.split()
     if(line[0]=='PING'):
+        timer = time.time()
+        print int(time.time()),'PONG '+line[1]
         s.send('PONG '+line[1]+'\n')
+    if(time.time()-timer>900):
+        print int(time.time()),'no PING for 15 min, exiting..'
+        sys.exit(1)
